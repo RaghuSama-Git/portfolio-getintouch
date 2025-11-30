302461bb8eddcaa5751e9668d1a81d6f7947e42a
RaghuSama-Git <raghusama99@gmail.com>
2025-11-30 11:09:55 +0530

    just project setup - no major code changes

---
+++ src/test/java/com/portfolio/get_in_touch/GetInTouchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.get_in_touch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetInTouchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
